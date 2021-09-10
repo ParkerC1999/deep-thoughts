5eabdd8e85ed66f882aeada2f6409fedb70030c1
ParkerC1999 <parkman.christiansen@gmail.com>
2021-09-09 18:02:32 -0600

    client folder

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+
+const path = require('path');
+
+const { authMiddleware } = require('./utils/auth');
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -11,7 +16,8 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
@@ -23,6 +29,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
